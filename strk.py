6030d699e5684cdead3c096b6edf387d40b35c84
catweazle <matthias.schoener@gmail.com>
2020-01-14 17:27:11 -0800

    added streak toy routines

---
+++ strk.py
@@ -0,0 +1,6 @@
+## this is a toy program to evaluate a couple of 'streaks'
+## solutions that were proposed on stackoverflow
+## https://stackoverflow.com/questions/35427298/finding-streaks-in-pandas-dataframe
+import pandas as pd
+
+df = pd.read_csv('strk.csv', header=0, delim_whitespace=True)
