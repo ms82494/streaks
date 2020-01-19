114f4ccd518b4604017756318115599c2f43c0d1
ms82494 <6218876+ms82494@users.noreply.github.com>
2020-01-18 16:17:09 -0800

    added R2K stocks, eliminated zero volume streaks

--- gettickers.py
+++ gettickers.py
@@ -1,25 +1,36 @@
-import requests
 import pandas as pd
 import sys
 
+
 def conv(s):
-	s = s.replace(' ','')
-	if s in ['BRKA', 'BFB']:
-		s = s[:-1] + '-' + s[-1]
-	return s
+    s = s.replace(' ', '')
+    # on the following 3 tickers tiingo deviates from the norm of
+    # letting the share class identifier follow the symbol root
+    # (e.g. Alphabet Class L => 'GOOGL'), instead inserting a '-' between
+    # the root and the class identifier.
+    if s in ['BRKA', 'BFB', 'LGFA']:
+        s = s[:-1] + '-' + s[-1]
+    # For the purpose of this exercise I don't want preferred shares (which)
+    # should not show up anyway.
+    if s in ['PSBPRZ']:
+        s = s[:3]
+    return s
+
+
 def read_index_table(idx):
-	fname = idx + '.xlsx'
-	try:
-		df = pd.read_excel(fname, headers=0, converters={'Ticker': conv})
-	except Exception:
-		print(f'could not open file {fname}')
-		sys.exit(0)
-	return df.Ticker.tolist()
+    fname = './Indices/' + idx + '.xlsx'
+    try:
+        df = pd.read_excel(fname, headers=0, converters={'Ticker': conv})
+    except Exception:
+        print(f'could not open file {fname}')
+        sys.exit(0)
+    return df.Ticker.tolist()
+
 
 def get_tickers():
-	indices = ['spx','qqq']
-	tickers = []
-	for ix in indices:
-		t = read_index_table(ix)
-		tickers.extend(t)
-	return list(set(tickers))
+    indices = ['spx', 'qqq', 'russell3000']
+    tickers = []
+    for ix in indices:
+        t = read_index_table(ix)
+        tickers.extend(t)
+    return list(set(tickers))
--- strk.py
+++ strk.py
@@ -2,35 +2,37 @@ import pandas as pd
 import numpy as np
 import gettickers as gt
 
+
 def ticker_streaks(ticker):
-    fname = ticker + '.csv'
-    df = pd.read_csv(fname, header=0, usecols=['date', 'adjClose'])
-    
+    fname = './CSVs/' + ticker + '.csv'
+    df = pd.read_csv(fname, header=0,
+                     usecols=['date', 'adjClose', 'adjVolume'])
+    df.drop(0, inplace=True)
+    df = df[df['adjVolume'] != 0]
     df['returnFactor'] = 1 + df.adjClose.pct_change(1)
     df['retNextMonth'] = 1/(df.adjClose.pct_change(-4) + 1) - 1
-    df['up'] = (df['returnFactor'] >=1).astype(int)
-    
-    df.drop(0, inplace=True)
+    df['up'] = (df['returnFactor'] > 1).astype(int)
 
     df['switch'] = abs(df['up'] - df['up'].shift(periods=1))
-    
+
     df['strkno'] = df.index
-    df.loc[df['switch'] ==0, 'strkno'] = np.nan
-    df.loc[1,'strkno'] = 0
+    df.loc[df['switch'] == 0, 'strkno'] = np.nan
+    df.loc[1, 'strkno'] = 0
     df['strkno'] = df['strkno'].fillna(method='ffill')
-    
+
     g = df.groupby('strkno').agg(
-        startdate = pd.NamedAgg(column='date', aggfunc='min'),
-        enddate = pd.NamedAgg(column='date', aggfunc='max'),
-        length = pd.NamedAgg(column='date', aggfunc='count'),
-        ret = pd.NamedAgg(column='returnFactor',
-                          aggfunc=lambda x: x.prod() - 1),
-        fwd_ret = pd.NamedAgg(column='retNextMonth', aggfunc='last'),
-        up = pd.NamedAgg(column='up', aggfunc='max'))
+        startdate=pd.NamedAgg(column='date', aggfunc='min'),
+        enddate=pd.NamedAgg(column='date', aggfunc='max'),
+        length=pd.NamedAgg(column='date', aggfunc='count'),
+        ret=pd.NamedAgg(column='returnFactor',
+                        aggfunc=lambda x: x.prod() - 1),
+        fwd_ret=pd.NamedAgg(column='retNextMonth', aggfunc='last'),
+        up=pd.NamedAgg(column='up', aggfunc='max'))
     g.reset_index(inplace=True)
     g.drop('strkno', axis=1, inplace=True)
     g['ticker'] = ticker
     return g
 
+
 allstreaks = pd.concat([ticker_streaks(s) for s in gt.get_tickers()],
-                ignore_index=True)
+                       ignore_index=True)
