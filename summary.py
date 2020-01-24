af5f2f36a3efcfe4eb704902aa957a9cdf1ef396
catweazle <matthias.schoener@gmail.com>
2020-01-23 16:31:41 -0800

    added stuff to summary

--- summary.py
+++ summary.py
@@ -8,6 +8,7 @@ Created on Wed Jan 22 21:19:33 2020
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import table
 
 df1 = pd.read_csv('streaks.csv', header=0)
 df1 = df1[df1['length']!=0]
@@ -27,14 +28,23 @@ def binned(x):
 
 df1['weeks'] = df1['length'].apply(binned)
 
-table = pd.pivot_table(df1,
+tbl = pd.pivot_table(df1,
                        values=['startdate'],
-                       index = ['bins'],
+                       index = ['weeks'],
                        columns = ['up'],
                        aggfunc = 'count',
                        fill_value=0)
 
-table = table.reindex([binned(x) for x in [1,2,3,4,5,8,10,15]])
-table=table.rename(columns={'startdate': 'streaks',
+tbl = tbl.reindex([binned(x) for x in [1,2,3,4,8,10,15]])
+tbl=tbl.rename(columns={'startdate': 'streaks',
                             0.0: 'No',
                             1.0: 'Yes'})
+
+#from https://stackoverflow.com/questions/35634238/how-to-save-a-pandas-dataframe-table-as-a-png
+ax = plt.subplot(111, frame_on=False) # no visible frame
+ax.xaxis.set_visible(False)  # hide the x axis
+ax.yaxis.set_visible(False)  # hide the y axis
+
+table(ax, tbl)  # where tbl is your data frame
+
+plt.savefig('./Images/mytable.png')
